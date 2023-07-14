9a7fa665cd87a4a8f8b4e92c31fb443538b44dc5
FRANCISCO <pachonetworking@gmail.com>
2023-07-14 01:28:50 -0500

    gridlayoud finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
