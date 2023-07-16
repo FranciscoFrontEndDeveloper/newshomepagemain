5ba7ee0ecf6875a3dd279f338cbfed06be0366b2
FRANCISCO <pachonetworking@gmail.com>
2023-07-15 23:57:31 -0500

    componet menu title news tops created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { TitleComponent } from './components/title/title.component';
+import { NewsComponent } from './components/news/news.component';
+import { TopsComponent } from './components/tops/tops.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    HomepageComponent
+    HomepageComponent,
+    MenuComponent,
+    TitleComponent,
+    NewsComponent,
+    TopsComponent
   ],
   imports: [
     BrowserModule,
